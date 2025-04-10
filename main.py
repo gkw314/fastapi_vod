d0f3491c51f46975fc155c1cc9f1cf76340ae490
gkw314 <go980314@gmail.com>
2025-04-10 13:34:28 +0900

    install mypy

--- main.py
+++ main.py
@@ -4,5 +4,10 @@ app = FastAPI()
 
 
 @app.get("/")
-async def root():
+async def root() -> dict[str, str]:
     return {"message": "안녕하세요, when2meet 앱입니다!"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str) -> dict[str, str]:
+    return {"message": f"Hello {name}"}
--- temp.py
+++
@@ -1,6 +0,0 @@
-# 특정 라인에 대해서 lint를 하고 싶지 않을때 # noqa
-
-# import temp2 #noqa
-
-import os
-import sys
